867d82b81550faa37c4c55e2c6178bea5c740fba
mid0111 <m.tajima0111@gmail.com>
2015-09-06 15:38:07 +0900

    Emacs port の 設定でCommandキーをmetaとするように修正

--- home/.spacemacs.d/private/mid0111/keybindings.el
+++ home/.spacemacs.d/private/mid0111/keybindings.el
@@ -1,3 +1,7 @@
+;; Treat command as meta
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+
 (define-key global-map (kbd "C-c C-g") 'helm-ls-git-ls)
 (define-key global-map (kbd "C-c g") 'helm-git-grep)
 (define-key global-map (kbd "C-c M-g") 'helm-git-grep-at-point)
