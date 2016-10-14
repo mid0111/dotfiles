96b1237f3200f4f27d284999271f8b06ae5bd348
mid0111 <m.tajima0111@gmail.com>
2016-10-14 19:28:28 +0900

    emacs の meta と command の入れ替えをやめた

--- home/.spacemacs.d/private/mid0111/keybindings.el
+++ home/.spacemacs.d/private/mid0111/keybindings.el
@@ -1,7 +1,3 @@
-;; Treat command as meta
-(setq mac-command-key-is-meta t)
-(setq mac-command-modifier 'meta)
-
 (define-key global-map (kbd "C-c C-g") 'helm-ls-git-ls)
 (define-key global-map (kbd "C-c g") 'helm-git-grep)
 (define-key global-map (kbd "C-c M-g") 'helm-git-grep-at-point)
